581798d6aa92a039d40a106666534f495e295202
Kai Zhu <rengwuxian@gmail.com>
2021-02-27 23:15:36 +0800

    Fix lint error: wildcard import in DogDetails.kt

--- app/src/main/java/com/example/androiddevchallenge/ui/DogDetails.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/DogDetails.kt
@@ -2,7 +2,11 @@ package com.example.androiddevchallenge.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
